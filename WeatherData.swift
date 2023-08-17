f00d243fcc79153ce84fc9c67216b866cbbfbe52
eocb <eduardocabral95@gmail.com>
2023-08-17 17:01:56 -0300

    API Decode

---
+++ WeatherData.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Cabral Costa, Eduardo on 17/08/23.
+//  Copyright © 2023 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData: Decodable{
+    let name: String
+}
--- WeatherManager.swift
+++ WeatherManager.swift
@@ -13,14 +13,36 @@ struct WeatherManager{
 
     func fetchWeather(cityName: String){
         let urlString = "\(weatherURL)&q=\(cityName)"
-        print(urlString)
+        performRequest(urlString: urlString)
     }
 
     func performRequest(urlString: String){
         //1. Create Url
-        let url = URL(string: urlString)
-        //2. Create a URLSession
-        //3. Give the session a task
-        //4. Start the task
+        if let url = URL(string: urlString){
+            //2. Create a URLSession
+            let session = URLSession(configuration: .default)
+            //3. Give the session a task
+            let task = session.dataTask(with: url) { data, response, error in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                if let safeData = data {
+                    self.parseJSON(weatherData: safeData)
+                }
+            }
+            //4. Start the task
+            task.resume()
+        }
+    }
+
+    func parseJSON(weatherData: Data){
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            print(decodedData.name)
+        } catch{
+            print(error)
+        }
     }
 }
