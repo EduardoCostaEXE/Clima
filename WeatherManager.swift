5ef1c1f15ea4cf76e8d8c2db0d4ea9657a418a17
eocb <eduardocabral95@gmail.com>
2023-08-17 17:46:13 -0300

    WeatherModel

--- WeatherManager.swift
+++ WeatherManager.swift
@@ -40,9 +40,15 @@ struct WeatherManager{
         let decoder = JSONDecoder()
         do {
             let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
-            print(decodedData.name)
+            let id =  decodedData.weather[0].id
+            let temp = decodedData.main.temp
+            let name = decodedData.name
+
+            let weather = WeatherModel(conditionId: id, cityName: name, temperature: temp)
+
         } catch{
             print(error)
         }
     }
+    
 }
